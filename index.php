faeaa48685cec16b3fd60a4987b9e5662fdd5d4f
John Mangahis <jrafaelhm@gmail.com>
2022-05-26 00:26:49 +0100

    minor changes

--- index.php
+++ index.php
@@ -23,11 +23,11 @@
         <article id="about">
             <section class="about-topright">
                 <h1>Augmented reality has a problem</h1>
-                <p>The full potential of augmented reality is currently restricted by a lack of accessibility.  AR is currently only offered as a feature on certain websites or on company-specific apps. In order to offer both businesses and consumers the full use and entertainment potential of AR, these features need to be united into a single platform.</p>
+                <p>The full potential of augmented reality is restricted by a lack of accessibility. AR is currently only offered as a feature on certain websites or on company-specific apps. In order to offer both businesses and consumers the full use and entertainment potential of AR, these features need to be united into a single platform.</p>
             </section>
             <section class="about-botleft">
                 <h1>A single platform solution</h1>
-                <p>Mirage's platform creates a space for users to interact with augmented reality in a convenient and accessible way. The platform draws together existing AR features onto a single platform and implements local data transfer techniques to create interactive multi-user experiences that enhance daily life. The platform is a new medium for socialising and creates opportunities for companies and consumers alike. Businesses can make unique AR products and services, dynamic and insightful advertisements and interesting and engaging events whilst consumers can discover a space for creativity and entertainment.</p>
+                <p>Mirage's platform creates a space for users to interact with augmented reality in a convenient and accessible way. The platform draws together existing AR features onto a single platform and implements local data transfer techniques to create interactive multi-user experiences that enhance daily life. The platform is a new medium for socialising and creates opportunities for companies and consumers alike. Businesses can make unique AR products and services, dynamic advertisements and engaging events whilst consumers can discover a space for creativity and entertainment.</p>
             </section>
         </article>
         <article id="platform">
@@ -44,7 +44,7 @@
                         <div class="image-container"><img src="assets/images/store.svg" alt="marketplace-cart-icon"></div>
                         <div>
                             <h2>Marketplace</h2>
-                            <p>The platform's marketplace enables users to acquire AR items for personal use, or to enjoy with friends. Mirage encourages a creator economy and offers a new way for businesses and individuals to earn by creating AR digital assets. As well as inspiring creativity and innovation, this also ensures a diverse offering of products available to users on the marketplace.</p>
+                            <p>The marketplace enables users to acquire AR items and offers businesses and individuals the ability to earn through the sale of digital assets. Mirage also encourages a creator economy, inspiring creativity and innovation that ensures a diverse offering of products is available to users.</p>
                         </div>
                     </div>
                     <div class="container-item">
@@ -77,21 +77,21 @@
                     <div class="image-container"><img src="assets/images/team/josh.jpg" alt="joshua-venters"></div>
                     <div class="card-info">
                         <h3>Joshua Venters <span>| CTO</span></h3>
-                        <p>Joshua is a co-founder and has a background in industry where he specialised in machine learning, computer vision, R&D and deployment and server engineering. He is responsible for the technical development of Mirage's platform.</p>
+                        <p>Joshua is a co-founder and has a background in industry where he specialised in machine learning, computer vision and server engineering. He is responsible for the technical development of Mirage's platform.</p>
                     </div>
                 </div>
                 <div class="card">
                     <div class="image-container"><img src="assets/images/team/seb.jpg" alt="sebastian-pira"></div>
                     <div class="card-info">
                         <h3>Sebastian Pira <span>| Developer</span></h3>
-                        <p>Sebastian is a proven full stack engineer with a wealth of industry experience. Among his many roles, he is responsible for implementation of backend architecture on the Mirage platform.</p>
+                        <p>Sebastian is a full stack engineer with SaaS experience. He is responsible for implementation of backend architecture on the Mirage platform.</p>
                     </div>
                 </div>
                 <div class="card">
                     <div class="image-container"><img src="assets/images/team/behrad.jpg" alt="behrad-kooh"></div>
                     <div class="card-info">
                         <h3>Behrad Kooh <span>| Developer</span></h3>
-                        <p>Behrad has previous experience working at J.P. Morgan, and is a full stack engineer currently studying a PhD in networks. He is responsible for integrating vision algorithms and machine learning features to the platform.</p>
+                        <p>Behrad has previous experience working as a full stack engineer and is currently obtaining a PhD in networks. He is responsible for integrating vision algorithms and machine learning features to the platform.</p>
                     </div>
                 </div>
             </section>
